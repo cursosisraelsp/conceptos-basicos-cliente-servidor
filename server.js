0905514edbabb4c2dcc29090c3f7b1e7f2cab1a3
cursosisraelsp <cursos.israelsp@gmail.com>
2025-05-21 10:38:17 +0200

    Éste é o meu primeiro commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const cors = require('cors')
+const app = express();
+
+// use
+//app.use(cors());
+app.use(express.urlencoded({extended: true}))
+// Accedo o arquivo estático
+app.use(express.static(path.join(__dirname, "static")));
+app.post("/acceso", (req, res) => {
+    const {nome, email} = req.body;// desesctructura o obxeto de entrada
+    console.log(nome, email);
+    if(req.body.nome == 'Israel' && req.body.email == 'israel@israel.com'){
+        res.sendFile(path.join(__dirname, "/static/views/app.html"));
+    }else{
+        res.send("<h1>Usuario não autorizado</h1>"); 
+    }
+     
+});
+app.get("/enviousuario", (req, res) => {
+    res.send({usuario:{
+        nome:'Israel'
+    }})
+})
+//START SERVER
+app.listen(3000, function () {
+ console.log("Server running");
+});
