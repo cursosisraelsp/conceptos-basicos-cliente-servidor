7b4c1e8488c3dd61422acdab10cb4d213f017548
cursosisraelsp <cursos.israelsp@gmail.com>
2025-05-25 20:14:11 +0200

    Engadindo encriptación - entrar e sair

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
 const express = require("express");
 const path = require("path");
-const cors = require('cors')
+const cors = require('cors');
+const { accesoUser, isUser } = require("./0.CONTROLADORES/USUARIOS");
+const paxinas = require("./1.Datos/datos.paxinas");
 const app = express();
+
+// UTILIZO DOTENV : para iso debo escribir a seguinte liña
+require('dotenv').config();
 
 // use
 //app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+
 // Accedo o arquivo estático
 app.use(express.static(path.join(__dirname, "static")));
-app.post("/acceso", (req, res) => {
-    const {nome, email} = req.body;// desesctructura o obxeto de entrada
-    console.log(nome, email);
-    let condicionUsuarioCorrecto = req.body.nome == 'Israel' && req.body.email == 'israel@israel.com';
-    let datoEnviadoCondicionUsuarioCorrecto = {
-                resposta:"acesso autorizado",
-                usuario:{
-                    nome:'Israel'
-                }
-            }
-    let datoEnviadoEnErro = {resposta:"faltan campos ou usuario non rexistrado"}
 
+//// PETICIONS
 
-    if(condicionUsuarioCorrecto){    
-        res.send(datoEnviadoCondicionUsuarioCorrecto);
-    }else{
-        res.send(datoEnviadoEnErro); 
-    }
-     
-});
+app.post("/acceso", accesoUser);
 app.get("/app",(req,res)=>{
     res.sendFile(path.join(__dirname, "static/views/app.html"));
+})
+
+app.get("/paxina-app",isUser,(req,res)=>{
+    res.send(paxinas.app)
 })
 
 
