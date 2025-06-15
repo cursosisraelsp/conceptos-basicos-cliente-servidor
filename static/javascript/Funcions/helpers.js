9633a0769af66ca8f9ace927e28e47711b89f4d5
cursosisraelsp <cursos.israelsp@gmail.com>
2025-06-15 22:13:22 +0200

    Cambiando cousas

--- static/javascript/Funcions/helpers.js
+++ static/javascript/Funcions/helpers.js
@@ -1,3 +1,6 @@
+import { comunicacion } from "../comunicacionServer.js"
+import { insertoLista } from "../insertarLista.js"
+
 export const imaxesEliminarGardarEditar = () =>{
     let obxetoImaxes = {
         eliminar:"./imaxes/iconos/eliminar-50.png",
@@ -31,4 +34,31 @@ export const editarContidos = (arraySelectores,editar) => {
 }
 export const borrarElemento = (referencia) =>{
     referencia.remove()
+}
+export async function peticionHtmlApp(){
+
+                await comunicacion.peticionGetConToken()
+                insertoLista()
+
+                sair.addEventListener("click",()=>{
+                        console.log("sair")
+                        localStorage.removeItem("token");
+                        location.replace("/");
+                    })
+}
+export async function acceso() {
+    botonEnviar.addEventListener("click",async (e)=>{
+            e.preventDefault();
+
+            let resposta = await comunicacion.peticionPostFormulario()
+
+            if(resposta.resposta === "acesso autorizado"){
+                console.log("iremos a app")
+                localStorage.setItem("token",resposta.tokenUsuario)
+                location.replace("/app");
+            }else{
+                console.log(resposta.resposta);
+                mensagem.innerHTML = resposta.resposta;
+            }
+        })
 }
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,37 +1,10 @@
-import { comunicacion } from "./comunicacionServer.js"
-import { insertoLista } from "./insertarLista.js"
+import { acceso, peticionHtmlApp } from "./Funcions/helpers.js"
 
-
-async function peticionHtmlApp(){
-
-                await comunicacion.peticionGetConToken()
-                insertoLista()
-
-                sair.addEventListener("click",()=>{
-                        console.log("sair")
-                        localStorage.removeItem("token");
-                        location.replace("/");
-                    })
-}
-async function acceso() {
-    botonEnviar.addEventListener("click",async (e)=>{
-            e.preventDefault();
-
-            let resposta = await comunicacion.peticionPostFormulario()
-
-            if(resposta.resposta === "acesso autorizado"){
-                console.log("iremos a app")
-                localStorage.setItem("token",resposta.tokenUsuario)
-                location.replace("/app");
-            }else{
-                console.log(resposta.resposta);
-                mensagem.innerHTML = resposta.resposta;
-            }
-        })
-}
 if(location.pathname == "/app"){
     peticionHtmlApp()
+    
 }
 if(location.pathname == "/"){
     acceso()
+  
 }
